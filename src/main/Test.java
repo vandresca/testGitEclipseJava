2f4980ee49d5ce04019337cd75aae76eac126811
victor <102297884+vandresca@users.noreply.github.com>
2023-01-16 05:11:33 +0100

    cambio desde github 3

--- src/main/Test.java
+++ src/main/Test.java
@@ -4,14 +4,14 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola iverso!");
+		System.out.println("3333 Hola iverso!");
 		
-		System.out.println("Hi");
+		System.out.println("AHHi 3");
 		
-		int a =5;
-		float b= 3f;
+		int a =8;
+		double b= 3;
 		
-		System.out.println("Hola 2");
+		System.out.println("Adios 007");
 	}
 
 }
