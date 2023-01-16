6be7dbce0d826a64d301397400ea1c911904f402
victor <102297884+vandresca@users.noreply.github.com>
2023-01-16 04:58:21 +0100

    cambio desde github

--- src/main/Test.java
+++ src/main/Test.java
@@ -4,7 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola universo!");
+		System.out.println("Hola iverso!");
+		
+		System.out.println("Hi");
 		
 		System.out.println("Hola 2");
 	}
