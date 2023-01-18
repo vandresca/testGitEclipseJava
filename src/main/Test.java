3741e0da50f772ae53fd82b47fe6dad88e820540
Victor <victorac1981@gmail.com>
2023-01-18 14:02:46 +0100

    Cambios del desarrollador 1

--- src/main/Test.java
+++ src/main/Test.java
@@ -5,7 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ssssHi universo!");
-		System.out.println("Hoa");
+		System.out.println("Cambios desarrollador 1");
+		
+		
+		System.out.println("Más cambios del desarrollador 1");
 	}
 
 }
